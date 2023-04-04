b19e9df08464a3af99f7dddc98ffd1a3cd3833ec
Oswald <oswaldkam@gmail.com>
2023-04-04 01:43:41 -0400

    wip

--- Model.py
+++ Model.py
@@ -8,35 +8,35 @@ from torch.utils.data import (TensorDataset, DataLoader, RandomSampler,
 
 
 class Model:
-    TEST_SIZE = 0.3
-    RANDOM_SEED = 420
-    BATCH_SIZE = 50
 
     def __init__(self, token_data: DataFrame, class_data: DataFrame):
         self.token_data = token_data
         self.class_data = class_data
-        self.x_test, self.x_train, self.y_test, self.y_train = train_test_split()
+        self.x_test, self.x_train, self.y_test, self.y_train = self.train_test_split()
 
-    def train_test_split(self, test_size: float = TEST_SIZE, random_seed: int = RANDOM_SEED):
-        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(
+    def train_test_split(self, test_size: float = 0.3, random_seed: int = 420):
+        return train_test_split(
             self.token_data, self.class_data, test_size=test_size, random_state=random_seed)
 
-    def data_loader(self, train_inputs, val_inputs, train_labels, val_labels, batch_size=50):
-        # Convert data type to torch.Tensor
-        train_inputs, val_inputs, train_labels, val_labels =\
-            tuple(torch.tensor(data) for data in
-                  [train_inputs, val_inputs, train_labels, val_labels])
+    def min_max_pair(self):
+        return [[self.token_data.loc[:, col].min(), self.token_data.loc[:, col].max()] for col in self.token_data.columns]
 
-        # Create DataLoader for training data
-        train_data = TensorDataset(train_inputs, train_labels)
-        train_sampler = RandomSampler(train_data)
-        train_dataloader = DataLoader(
-            train_data, sampler=train_sampler, batch_size=self.BATCH_SIZE)
+    # def data_loader(self, train_inputs, val_inputs, train_labels, val_labels, batch_size=50):
+    #     # Convert data type to torch.Tensor
+    #     train_inputs, val_inputs, train_labels, val_labels =\
+    #         tuple(torch.tensor(data) for data in
+    #               [train_inputs, val_inputs, train_labels, val_labels])
 
-        # Create DataLoader for validation data
-        val_data = TensorDataset(val_inputs, val_labels)
-        val_sampler = SequentialSampler(val_data)
-        val_dataloader = DataLoader(
-            val_data, sampler=val_sampler, batch_size=self.BATCH_SIZE)
+    #     # Create DataLoader for training data
+    #     train_data = TensorDataset(train_inputs, train_labels)
+    #     train_sampler = RandomSampler(train_data)
+    #     train_dataloader = DataLoader(
+    #         train_data, sampler=train_sampler, batch_size=self.BATCH_SIZE)
 
-        return train_dataloader, val_dataloader
+    #     # Create DataLoader for validation data
+    #     val_data = TensorDataset(val_inputs, val_labels)
+    #     val_sampler = SequentialSampler(val_data)
+    #     val_dataloader = DataLoader(
+    #         val_data, sampler=val_sampler, batch_size=self.BATCH_SIZE)
+
+    #     return train_dataloader, val_dataloader
--- bag_of_word.py
+++ bag_of_word.py
@@ -1,13 +1,13 @@
 
+import neurolab as nl
 import os
 import numpy as np
 import pandas as pd
-import nltk
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.corpus import stopwords
 from sklearn.model_selection import train_test_split
-import torch
 from Model import Model
+import neurolab as nl
 
 from Preprocess import Preprocess, PreprocessName
 
@@ -17,9 +17,20 @@ TEST_SIZE = 0.4
 # Preprocess
 preprocess = Preprocess()
 lancaster_df = preprocess.get_tokenlized_df(PreprocessName.LANCASTER_STEMMER)
-lemmatize_df = preprocess.get_tokenlized_df(PreprocessName.LEMMATIZE)
-porter_df = preprocess.get_tokenlized_df(PreprocessName.PORTER_STEMMER)
-snowball_df = preprocess.get_tokenlized_df(PreprocessName.SNOWBALL_STEMMER)
+# lemmatize_df = preprocess.get_tokenlized_df(PreprocessName.LEMMATIZE)
+# porter_df = preprocess.get_tokenlized_df(PreprocessName.PORTER_STEMMER)
+# snowball_df = preprocess.get_tokenlized_df(PreprocessName.SNOWBALL_STEMMER)
 
+print(preprocess.data['CLASS'])
+print(lancaster_df)
 # Split datam into train and test
-lancaster_model = Model(lancaster_df, preprocess.data['CLASS'])
+model = Model(lancaster_df, preprocess.data['CLASS'])
+print("=======")
+print(model.x_train)
+print("=======")
+print(model.y_train)
+nn_ex1 = nl.net.newff(model.min_max_pair(), [100, 50, 25])
+error_progress_ex1 = nn_ex1.train(model.x_train, model.y_train,
+                                  epochs=1000, show=15, goal=0.00001)
+
+print(error_progress_ex1)
